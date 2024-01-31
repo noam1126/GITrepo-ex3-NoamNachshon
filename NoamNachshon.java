bdd4a9967262aa95fe5ec412cbb8d2a254289def
Noam Nachshon <noam1126@gmail.com>
2024-01-31 17:43:21 +0200

    upload

---
+++ NoamNachshon.java
@@ -0,0 +1,7 @@
+public class NoamNachshon{
+    public static void main(String[] args) {
+        for (int i=1;i<=10; i++) {
+            System.out.println("My name is Noam Nachshon");
+        }
+    }
+}
